fa4a99a3f694ab06e2365cde8cbdc38f71ed4986
Daniel Hara <haradaniel734@gmail.com>
2018-10-15 13:47:05 -0300

    printing selected value

--- app/forms/InsertOperationForm.py
+++ app/forms/InsertOperationForm.py
@@ -31,13 +31,14 @@ class InsertOperationForm(Form):
         choices = {'pineapples', 'bananas'}
         self.combo_box = self.make_combo_box(master, 'Strategy:', choices)
 
-        choices_2 = {'C', 'V'}
+        choices_2 = ('C', 'V')
         self.combo_box_2, self.chosen_2 = self.make_combo_box_2(master, choices_2)
 
         self.insert_operation_button = tk.Button(master, text="Insert Operation", command=self.insert_operation)
         self.insert_operation_button.pack()
 
     def insert_operation(self):
+        print('self.chosen_2=' + self.chosen_2.get())
         pass
 
 root = tk.Tk()
